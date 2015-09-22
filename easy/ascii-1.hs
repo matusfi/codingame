fa9aab2b39afe57f912b791c82f1b9899b2b00fa
Matus Laslofi <matus.laslofi@gmail.com>
2015-09-22 08:01:35 +0200

    WIP: almost there, but not really :(

--- easy/ascii-1.hs
+++ easy/ascii-1.hs
@@ -1,14 +1,17 @@
 import System.IO
 import Control.Monad
+import Data.Char (toUpper)
+import Data.List (transpose)
 import Data.List.Split (chunksOf)
 import qualified Data.Map.Strict as Map
 
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?"
 
-type PixelBank = [[String]]
-type CharMap = Map.Map Char [String]
+type PixelChar = [String]
+type PixelString = [PixelChar]
+type PixelCharMap = Map.Map Char PixelChar
 
-charMap :: String -> PixelBank -> CharMap
+charMap :: String -> PixelString -> PixelCharMap
 charMap abc pixelBank = Map.fromList $ zip abc pixelBank
 
 main :: IO ()
@@ -19,17 +22,47 @@ main = do
     let width = read input_line :: Int
     input_line <- getLine
     let height = read input_line :: Int
-    text <- getLine
+    text <- liftM (map toUpper) getLine
 
-    rows <- replicateM height $ do
+    pixelBank <- liftM transpose $ replicateM height $ do
       row <- getLine
       let splitRow = chunksOf width row
       return splitRow
 
+    -- errorCheck width height pixelBank
+    let chmap = charMap alphabet pixelBank
+
+
     -- hPutStrLn stderr "Debug messages..."
 
     -- Write answer to stdout
-    putStrLn "anwer"
+    putStrLn $ display chmap height text
 
-display :: CharMap -> String -> String
---display chmap text = unlines . blabla
+errorCheck :: Int -> Int -> PixelString -> IO ()
+errorCheck width height pixelBank
+  | length pixelBank /= height = error "Pixel Bank height doesn't match display height"
+  | any (\s -> length s /= width) (concat pixelBank) = error "Pixel Bank character width is not consistent with the expected width"
+  | otherwise = return ()
+
+display :: PixelCharMap -> Int -> String -> String
+display chmap height text = unlines . joinPixelLines (length text) . replaceWithPixelChars $ text
+  where
+    choosePixelChar :: Char -> PixelChar
+    choosePixelChar = (Map.!) chmap -- (Map.!) is unsafe Map key accessor
+
+    replaceWithPixelChars :: String -> PixelString
+    replaceWithPixelChars = map choosePixelChar
+
+    lineNumbers = [0..height-1]
+
+    joinPixelLines :: Int -> PixelString -> [String]
+    joinPixelLines len ps = map concat ps
+
+
+testMap :: PixelCharMap
+testMap = Map.fromList [('A', [" A  ",
+                               "AAA ",
+                               "A A "]),
+                        ('H', ["H H ",
+                               "HHH ",
+                               "H H "])]
